d3df8e01fc78621a117e3c08af302aacbf8ab6aa
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2020-11-30 16:56:18 +0500

    updated BMI project

--- BMI Calculator - Mini Project/bmi.py
+++ BMI Calculator - Mini Project/bmi.py
@@ -3,7 +3,7 @@
 # 30 Nov 2020
 
 name = input("Enter your name: ")
-mass = int(input("Enter your mass (in kg): ")) # mass = '64' , mass = 64
+mass = int(input("Enter your mass (in kg): "))
 height = int(input("Enter your height (in cms): "))
 
 height = height / 100 # converting height from centimeters into meters
